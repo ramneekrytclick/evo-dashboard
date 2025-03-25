9fad7640a6dcf6ad2fbf02bdcad02b39b94dff6d
Ramneek Singh <singhrramneek@gmail.com>
2025-03-25 18:59:38 +0530

    Fetch and display student profile data in dashboard: implement API call for student profile, handle errors with toast notifications, and add dashboard content structure

--- src/Components/Student/Dashboard/index.tsx
+++ src/Components/Student/Dashboard/index.tsx
@@ -1,7 +1,26 @@
+"use client";
+import { getStudentProfile } from "@/app/api/student";
 import Breadcrumbs from "@/CommonComponent/BreadCrumbs";
 import { DashboardTitle, StudentTitle } from "@/Constant";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { Card, CardBody } from "reactstrap";
 
 const StudentDashboardContainer = () => {
+	const [evoScore, setScore] = useState();
+	const fetchData = async () => {
+		try {
+			const response = await getStudentProfile();
+			console.log(response);
+			setScore(response);
+		} catch (error: any) {
+			console.log(error.response.data.message);
+			toast.error(error.response.data.message);
+		}
+	};
+	useEffect(() => {
+		fetchData();
+	}, []);
 	return (
 		<>
 			<Breadcrumbs
@@ -9,6 +28,13 @@ const StudentDashboardContainer = () => {
 				parent={StudentTitle}
 				title={DashboardTitle}
 			/>
+			<Card>
+				<CardBody>
+					Student Dashboard Content
+					<p>Welcome, Student!</p>
+					{JSON.stringify(evoScore)}
+				</CardBody>
+			</Card>
 		</>
 	);
 };
